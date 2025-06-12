69bad69b894b032f197cb132bed063159f092489
shubham <sawantshubham736@gmail.com>
2025-06-13 02:02:27 +0530

    learning with api

--- Food-Restraurant/src/Components/Body.js
+++ Food-Restraurant/src/Components/Body.js
@@ -1,40 +1,33 @@
 import ResCard from "./ResCard.js";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { SWIIGGYAPI } from "../utils/constants.js";
 
 const Body = () => {
-  useEffect( () => {
+  let [restaurantList, setRestaurantList] = useState([]);
+
+  useEffect(() => {
     fetchData();
-  } , [] )
+  }, []);
 
   const fetchData = async () => {
     const data = await fetch(SWIIGGYAPI);
 
-    const json = data.json();
+    const json = await data.json();
 
     console.log(json);
-  }  
+
+    setRestaurantList(json?.data?.cards[3]?.card?.card?.info || []);
+  };
+
   return (
     <div>
       <div className="search" style={{ padding: "10px" }}>
         Search
       </div>
       <div className="bodyComponent">
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
+        {restaurantList.map((item) => (
+          <ResCard key={item.id} resData={item} />
+        ))}
       </div>
     </div>
   );
--- Food-Restraurant/src/utils/constants.js
+++ Food-Restraurant/src/utils/constants.js
@@ -5,4 +5,4 @@ export const RESCARDLOGO_URL =
   "https://as2.ftcdn.net/v2/jpg/04/36/36/57/1000_F_436365754_z3i5Es0sFmZuLY6GZIzdiU01v9HqpGZe.jpg";
 
 export const SWIIGGYAPI =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&collection=83631&tags=layout_CCS_Pizza&sortBy=&filters=&type=rcv2&offset=0&page_type=null";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&collection=83631&tags=layout_CCS_Pizza&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
